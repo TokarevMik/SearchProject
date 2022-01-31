1b5cfff52e00a926af34072192ca22fb5c1a44a7
TokarevMik <stiks84@gmail.com>
2022-01-31 19:20:58 +0500

    add node class

---
+++ src/main/java/main/Node.java
@@ -0,0 +1,18 @@
+
+package main;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import java.util.Collections;
+import java.util.HashSet;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.regex.Matcher;
+public class Node {
+        String url;
+        String text;
+//        private  Collections<Node> nodesList = new HashSet<>();
+//        private Collection<Node> nodes = new HashSet<>();
+}
